fc91e60974b16c3dd441e105fddeaa72e46376b8
Prince Kumar <Prince.Kumar@nectechnologies.in>
2019-11-04 12:46:48 +0530

    initiaal commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header-section/header-section.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-section',
+  templateUrl: './header-section.component.html',
+  styleUrls: ['./header-section.component.css']
+})
+export class HeaderSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  alertBox(){
+    alert('fsdfsd')
+  }
+
+}
